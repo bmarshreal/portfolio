5aa06b9c39b819ae35242edde80cab3f806c89f7
bmarshreal <bmarshreal@yahoo.com>
2021-02-27 19:17:55 -0600

    troubleshoot deployment issue floater

---
+++ src/components/ScrollFloater.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+function ScrollFloater(props) {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    function scroller() {
+      let scrollEX = Math.round(window.scrollY);
+      if (scrollEX > 100) {
+        setScroll(true);
+      }
+      // console.log(scroll);
+    }
+    window.addEventListener("scroll", scroller);
+    return () => {
+      console.log("Floater Mounted");
+      setScroll(false);
+    };
+  }, []);
+
+  function topScroll() {
+    window.scrollTo(0, 0);
+  }
+
+  return (
+    <div>
+      {scroll ? (
+        <button
+          className="floaterBtn"
+          onClick={topScroll}
+          type="button"
+
+          // onScroll={scroller}
+        >
+          ⇪
+        </button>
+      ) : null}
+    </div>
+  );
+}
+
+export default ScrollFloater;
