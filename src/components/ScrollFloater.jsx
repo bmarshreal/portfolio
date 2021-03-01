32d08aa389f0f7803c98a139b10c91bfabe2931a
bmarshreal <bmarshreal@yahoo.com>
2021-03-01 10:43:25 -0600

    fixing floater issue

--- src/components/ScrollFloater.jsx
+++ src/components/ScrollFloater.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function ScrollFloater(props) {
-  const [scroll, setScroll] = useState(false);
+  const [scroll, setScroll] = useState(true);
 
   useEffect(() => {
     function scroller() {
