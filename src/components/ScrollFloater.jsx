8e8c55acec525c778720edc123a1b51e3010be7b
bmarshreal <bmarshreal@yahoo.com>
2021-02-28 20:31:29 -0600

    fixing floater issue

--- src/components/ScrollFloater.jsx
+++ src/components/ScrollFloater.jsx
@@ -52,7 +52,7 @@ function ScrollFloater(props) {
             style={{
               position: "relative",
               bottom: "55%",
-              right: "9%",
+              right: "10%",
             }}
           >
             {" "}
